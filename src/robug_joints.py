267b6b9ad62c582948246a92239bf6967752aed3
RobotsForAll <alexander.fritsch@gmx.com>
2026-01-12 01:46:03 +0100

    Add GPL licensing information
    Added licensing information to the top of the file.

--- src/robug_joints.py
+++ src/robug_joints.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2026 RobotsForAll
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from machine import Pin, PWM
 from time import sleep
 from robug_constants import constants as c
@@ -52,3 +67,4 @@ class rbjoints:
     def set_angles(self, iDeltaTicks, iGammaTicks):
         self.set_angle_sid(0,  iDeltaTicks)
         self.set_angle_sid(1,  iGammaTicks)         
+
