6cdb286724470af97f08c62367565084e273da62
RobotsForAll <alexander.fritsch@gmx.com>
2026-01-12 01:43:42 +0100

    Refactor rbgait class and add GPL license

--- src/robug_gait.py
+++ src/robug_gait.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2026 RobotsForAll
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from math import pi, sin
 from robug_utils import v3
 from robug_constants import constants as c
@@ -271,4 +286,5 @@ class rbgait:
         self.bPush = True
         
     def push_disable(self):
+
         self.bPush = False
